4a70ae3d8847aa6507ca1242323a55c4f76a34f1
Alex Camaroti <camarottii@hotmail.com>
2023-03-04 20:14:55 -0300

    Incluido base project

---
+++ src/main/java/br/com/cammarota/alex/ifittrack/IfittrackApplication.java
@@ -0,0 +1,13 @@
+package br.com.cammarota.alex.ifittrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IfittrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IfittrackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/cammarota/alex/ifittrack/IfittrackApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cammarota.alex.ifittrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IfittrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
